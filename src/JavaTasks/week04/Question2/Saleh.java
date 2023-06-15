e7149d6c67bbbabce1deed2073b0a099c2abce3c
SalehGamaly <123895230+SalehGamaly@users.noreply.github.com>
2023-06-15 12:20:24 -0400

    Saleh Q2

---
+++ src/JavaTasks/week04/Question2/Saleh.java
@@ -0,0 +1,35 @@
+package JavaTasks.week04.Question2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Saleh {
+    /*
+     String -- Remove Duplicates
+     Write a return method that can remove the duplicated values from String
+     Ex: removeDup("AAABBBCCC") ==> ABC
+     */
+
+    public static String removeDub(String str){
+        Set<Character> set = new HashSet<>();
+
+        for (char eachChar : str.toCharArray()) {
+            set.add(eachChar);
+        }
+
+        String newStr = "";
+        for (Character each : set) {
+            newStr += each;
+        }
+        return newStr;
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("removeDub(\"AAABBBCCC\") = " + removeDub("AAABBBCCC"));
+    }
+}
