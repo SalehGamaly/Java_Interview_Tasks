e6f4a4f5457812f4fa30b6fbaa6282d2042ee79e
SalehGamaly <123895230+SalehGamaly@users.noreply.github.com>
2023-06-15 12:19:20 -0400

    Created main method

--- src/JavaTasks/week04/Question1/Saleh.java
+++ src/JavaTasks/week04/Question1/Saleh.java
@@ -1,7 +1,26 @@
 package JavaTasks.week04.Question1;
 
-public class Saleh {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Saleh {
+   /* ➡️ String -- Same letters
+    Write a return method that check if a string is build out of the same letters as another string.
+    Ex: same("abc", "cab"); -> true
+    same("abc", "abb"); -> false:
+
+    */
+
+    public static void main(String[] args) {
+        System.out.println("sameLetters = " + sameLetters("tea", "eat"));
+    }
+    public static boolean sameLetters(String str1, String str2) {
+
+        char[] charArr1 = str1.toCharArray();
+        char[] charArr2 = str2.toCharArray();
+
+        Arrays.sort(charArr1);
+        Arrays.sort(charArr2);
+
+        return Arrays.equals(charArr1, charArr2);
     }
 }
