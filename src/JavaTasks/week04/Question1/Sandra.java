31cc3fbdcea162be28d34813a439c9ee4d3880fc
Sandra_Cydeo <sirmansandramaria@gmail.com>
2023-06-15 12:37:33 -0400

    Changes to the tasks - Sandra

--- src/JavaTasks/week04/Question1/Sandra.java
+++ src/JavaTasks/week04/Question1/Sandra.java
@@ -28,7 +28,7 @@ public class Sandra {
 
     public static void main(String[] args) {
 
-        System.out.println(same("tree", "rwet"));
+        System.out.println(same("tree", "reet"));
 
     }
 
