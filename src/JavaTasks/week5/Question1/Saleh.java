1b2950c02e80b307337750e86cc4c9fa082f6727
Saleh Gamaly <123895230+SalehGamaly@users.noreply.github.com>
2023-06-22 13:30:34 -0400

    Update Saleh.java

--- src/JavaTasks/week5/Question1/Saleh.java
+++ src/JavaTasks/week5/Question1/Saleh.java
@@ -9,43 +9,5 @@ public class Saleh {
     Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
      */
 
-    public static void main(String[] args) {
-
-        //System.out.println("uniqueChar1(\"AAABBBCCCDEF\") = " + uniqueChar("AAABBBCCCDEF"));
-
-        System.out.println("uniqueChar2(\"AAABBBCCCDEF\") = " + uniqueChar2("AAABBBCCCDEF"));
-    }
-
-    public static String uniqueChar1(String str){
-
-        String uniqueStr = "";
-
-        Set<Character> strSet = new HashSet<>();
-
-        for (char each : str.toCharArray()) {
-            strSet.add(each);
-        }
-
-        for (Character character : strSet) {
-            uniqueStr += character;
-        }
-        return uniqueStr;
-    }
-
-    public static String uniqueChar2(String str){
-
-        String uniqueChars = "";
-
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = 0; j < str.length(); j++) {
-                if ( !(uniqueChars.contains(str.charAt(i) + ""))){
-                    uniqueChars += str.charAt(i);
-                }
-            }
-        }
-        return uniqueChars;
-    }
-
-
 
 }
