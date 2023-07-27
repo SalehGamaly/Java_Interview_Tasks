f64f9cb865d9a827062631b417a3f27601f86567
SalehGamaly <123895230+SalehGamaly@users.noreply.github.com>
2023-07-27 20:18:12 +0300

    Saleh week 09 - Q 1, 2, 3

--- src/JavaTasks/week9/Question3/Saleh.java
+++ src/JavaTasks/week9/Question3/Saleh.java
@@ -2,6 +2,7 @@ package JavaTasks.week9.Question3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Saleh {
@@ -9,7 +10,9 @@ public class Saleh {
     public static void main(String[] args) {
 
         ArrayList<Integer> values = new ArrayList<>(Arrays.asList(200, 20, 30, 150, 90, 100, 110, 55));
-        System.out.println(removeIntegerFromList(values));
+        System.out.println("Solution 1: " + removeIntegerFromList(values));
+        System.out.println("Solution 2: " + removeIntegerFromList2(values));
+        System.out.println("Solution 3: " + removeIntegerFromList3(values));
 
     }
 
@@ -27,7 +30,26 @@ public class Saleh {
         return resultList;
     }
 
-}
+    public static List<Integer> removeIntegerFromList2(List<Integer> integerList) {
+
+        Iterator<Integer> iterator = integerList.iterator();
+
+        while (iterator.hasNext()){
+            if (iterator.next() > 100){
+                iterator.remove();
+            }
+        }
+        return integerList;
+    }
+
+    public static List<Integer> removeIntegerFromList3(List<Integer> integerList) {
+
+        integerList.removeIf(p -> p > 100);
+
+        return integerList;
+    }
+
+    }
     /*
     ArrayList - Remove some values
   Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
