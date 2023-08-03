51c2b4661a070191908b76666b35c1b16ace6068
SalehGamaly <123895230+SalehGamaly@users.noreply.github.com>
2023-08-03 11:01:55 +0300

    Saleh week 09 - Q 1, 2, 3

---
+++ src/JavaTasks/week10/Question2/Saleh.java
@@ -0,0 +1,49 @@
+package JavaTasks.week10.Question2;
+
+import java.util.ArrayList;
+
+public class Saleh {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(10);
+        arrayList.add(5);
+        arrayList.add(2);
+        arrayList.add(7);
+        arrayList.add(3);
+        arrayList.add(1);
+        arrayList.add(8);
+        arrayList.add(9);
+        arrayList.add(6);
+
+        System.out.println(sortedArrList(arrayList));
+    }
+
+    public static ArrayList<Integer> sortedArrList(ArrayList<Integer> arrayList){
+
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+
+            for (int j = i + 1; j < arrayList.size(); j++) {
+
+                int arrListi = arrayList.get(i);
+                int arrListj = arrayList.get(j);
+                int temporary = arrayList.get(i);
+
+                if (arrListi < arrListj) {
+                    arrayList.set(i, arrListj);
+                    arrayList.set(j, temporary);
+                }
+
+            }
+        }
+        return arrayList;
+    }
+
+}
+/*
+2. ArrayList - sorting in descending order
+  Write a method that can sort the ArrayList in descending order without
+using the sort method.
+
+ */
---
+++ src/JavaTasks/week10/Question3/Saleh.java
@@ -0,0 +1,8 @@
+package JavaTasks.week10.Question3;
+
+public class Saleh {
+}
+/*
+3. Map - Sort the map by values
+  Write a method that can sort the Map by values.
+ */
